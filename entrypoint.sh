5e8362094cda483779ddff9839e4081c0e9ba71d
miremi9 <remi.michot37@gmail.com>
2026-07-24 22:08:21 +0200

    add init

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 uv run python manage.py migrate
 uv run python manage.py collectstatic --noinput
+uv run python manage.py init_data
 
 exec uv run gunicorn config.wsgi:application --bind 0.0.0.0:8000
