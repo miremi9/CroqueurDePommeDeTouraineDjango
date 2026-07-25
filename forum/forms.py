32d8f5affe516990e93f7217dea74522fd660e45
miremi9 <remi.michot37@gmail.com>
2026-07-25 09:28:08 +0200

    support iframe

--- forum/forms.py
+++ forum/forms.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout
 from django import forms
@@ -20,3 +21,13 @@ class ArticleForm(forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.layout = Layout('title', 'content', 'file')
+
+    def clean_content(self):
+        content = self.cleaned_data["content"]
+
+        soup = BeautifulSoup(content, "html.parser")
+
+        for iframe in soup.find_all("iframe"):
+            iframe.attrs.pop("sandbox", None)
+
+        return str(soup)
